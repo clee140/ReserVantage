fead5ab1cc9428a1f7bf6885775e907f0f8a2d52
sruthi-lingam12 <143719881+sruthi-lingam12@users.noreply.github.com>
2023-12-01 20:27:35 -0500

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.io.*;
 import java.net.Socket;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Client extends JComponent implements Runnable {
     JLabel welcomeMessageLabel = new JLabel("Welcome to the Hotel Manager!");
